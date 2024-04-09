323d4891be92448aa475d3b6b28b9ddb56e1b7f3
Paul Hondola <paulhondola@gmail.com>
2024-04-09 12:30:27 +0300

    stack_list lib ready

--- stack_list/main.c
+++ stack_list/main.c
@@ -1,2 +1,20 @@
 #include "stack_list.h"
-int main(void) { return 0; }
+int main(void) {
+
+  stack_list_t st = init_stack();
+
+  for (int i = 0; i < 5; i++)
+    st = push(st, i);
+
+  print_stack(st);
+
+  st = pop(st);
+
+  printf("PEEK: %d\n", peek(st));
+
+  print_stack(st);
+
+  free_stack(st);
+
+  return 0;
+}
--- stack_list/stack_list.h
+++ stack_list/stack_list.h
@@ -3,4 +3,31 @@
 
 #include "/Users/paulhondola/Faculta/Tehnici de Programare/list/list.h"
 
+typedef list_data_t payload_t;
+typedef struct STACK_LIST *stack_list_t;
+
+// INIT
+stack_list_t init_stack(void);
+
+// FREE
+void free_stack(stack_list_t stack);
+
+// DISPLAY
+void print_stack(stack_list_t stack);
+
+// SIZE
+size_t get_size(stack_list_t stack);
+
+// PUSH
+stack_list_t push(stack_list_t stack, payload_t info);
+
+// POP
+stack_list_t pop(stack_list_t stack);
+
+// PEEK
+payload_t peek(stack_list_t stack);
+
+// ISEMPTY
+int is_empty(stack_list_t stack);
+
 #endif
