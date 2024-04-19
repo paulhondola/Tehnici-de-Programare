23f3f06ad563ce19e0e8332ee3376804af64b309
Paul Hondola <paulhondola@gmail.com>
2024-04-19 23:55:51 +0300

    curs 9 update

---
+++ curs/curs9/hanoi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void hanoi(int n, char src, char aux, char dest)
+{
+	if(n == 1)
+	{
+		printf("%c --> %c\n", src, dest);
+		return;
+	}
+
+	hanoi(n-1, src, dest, aux);
+	printf("%c --> %c\n", src, dest);
+	hanoi(n - 1, aux, src, dest);
+}
+
+int main(void)
+{
+	hanoi(3, 'A', 'B', 'C');
+	return 0;
+}
