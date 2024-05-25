d7ef91b70754daba0dd0679b7fc6daf00163a2b8
Paul Hondola <paulhondola@gmail.com>
2024-05-25 15:10:26 +0300

    test/p2/2 update

--- test/p2/2/main.c
+++ test/p2/2/main.c
@@ -15,3 +15,68 @@ Numarul tau este 37?
 >0
 Numarul ales de tine a fost gasit.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MIN_NUMBER 1
+#define MAX_NUMBER 100
+typedef enum { OK = 0, LOW = 1, HIGH = 2, ERR = -1 } binary_search_codes_t;
+
+binary_search_codes_t enter_option(void) {
+
+  binary_search_codes_t code = ERR;
+
+  printf("Input option: ");
+  scanf("%d", &code);
+
+  switch (code) {
+  case OK:
+    break;
+  case LOW:
+    break;
+  case HIGH:
+    break;
+  default:
+    printf("Invalid option!\n");
+    return enter_option();
+  }
+
+  return code;
+}
+
+void search_number(int number, int low, int high) {
+
+  int mid = (low + high) / 2;
+  printf("Is your number %d?\n", mid);
+
+  binary_search_codes_t option = enter_option();
+
+  switch (option) {
+  case OK:
+    printf("Your number is %d!\n", mid);
+    return;
+  case LOW:
+    printf("Lower\n");
+    search_number(number, low, mid);
+    break;
+  case HIGH:
+    printf("Higher\n");
+    search_number(number, mid, high);
+    break;
+  default:
+    perror("ERROR");
+    break;
+  }
+}
+
+int main(int argc, char **argv) {
+
+  if (argc != 2)
+    return 1;
+
+  int number = atoi(argv[1]);
+
+  search_number(number, MIN_NUMBER, MAX_NUMBER);
+
+  return 0;
+}
