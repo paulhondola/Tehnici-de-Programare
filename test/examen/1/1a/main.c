fb30885d60e3a2376f88dd8fb27873f9d4d90629
Paul Hondola <paulhondola@gmail.com>
2024-06-07 18:12:40 +0300

    moved examen folders to subiecte 2021

---
+++ test/examen/1/1a/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+1. Se citesc dintr-un fisier transmis ca si parametru in linie de comanda, un
+numar natural n (n<=100000) si apoi n numere intregi. Se parcurge vectorul
+format din aceste numere, salvand sumele elementelor consecutive. Dupa fiecare
+parcurgere se afiseaza numarul iteratiei si vectorul rezultat. Se va repeta
+acest pas pana cand vectorul va contine un singur numar. Exemplu
+
+intrare:
+
+5
+2 5 7 -3 9
+
+Afiseaza:
+Iteratia 1: 7 12 4 6
+Iteratia 2: 19 16 10
+Iteratia 3: 35 26
+Iteratia 4: 61
+*/
