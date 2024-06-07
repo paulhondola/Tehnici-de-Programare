af76198d32aded8d60a7c6281abb2882765ea9c4
Paul Hondola <paulhondola@gmail.com>
2024-06-07 18:43:48 +0300

    added examen / 1/ 1a

--- test/examen/1/1a/main.c
+++ test/examen/1/1a/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
 1. Se citesc dintr-un fisier transmis ca si parametru in linie de comanda, un
@@ -18,3 +19,30 @@ Iteratia 2: 19 16 10
 Iteratia 3: 35 26
 Iteratia 4: 61
 */
+
+void print_array(int array[], int size) {
+  for (int i = 0; i < size; i++)
+    printf("%d ", array[i]);
+  printf("\n");
+}
+
+void rec_array_sum(int array[], int size) {
+  if (size < 0)
+    return;
+
+  print_array(array, size);
+  for (int i = 0; i < size - 1; i++)
+    array[i] = array[i] + array[i + 1];
+
+  return rec_array_sum(array, size - 1);
+}
+
+int main(void) {
+
+  int size = 5;
+  int array[] = {2, 5, 7, -3, 9};
+
+  rec_array_sum(array, size);
+
+  return 0;
+}
