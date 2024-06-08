32bb5be4ae867558e327381733e3731d97876789
Paul Hondola <paulhondola@gmail.com>
2024-06-08 16:54:41 +0300

    updated examen 5

--- examen/5/main.c
+++ examen/5/main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /*
 Se dau N dame şi o tablă de şah de dimensiune NxN. Să se găsească toate
 modalităţile de a aranja toate damele astfel încât oricare două dame să nu se
@@ -35,3 +38,68 @@ solutii.
 
 Problema se va rezolva utilizand un algoritm de backtracking.
 */
+#define STACK_INIT_VALUE 0
+
+int successor(int stack[], int level, int size) {
+  if (stack[level] >= size)
+    return 0;
+
+  stack[level]++;
+  return 1;
+}
+
+int valid(int stack[], int level) {
+  for (int i = 1; i < level; i++)
+    if (abs(level - i) == abs(stack[level] - stack[i]) ||
+        stack[i] == stack[level])
+      return 0;
+  return 1;
+}
+
+int solution(int level, int size) { return level == size; }
+
+void print_solution(int stack[], int size) {
+  for (int i = 1; i <= size; i++)
+    printf("%d ", stack[i]);
+  printf("\n");
+}
+
+int back(int size) {
+  int permutations = 0;
+  int stack[size + 1];
+  int level = STACK_INIT_VALUE + 1;
+  stack[0] = STACK_INIT_VALUE;
+  stack[1] = STACK_INIT_VALUE;
+
+  while (level > STACK_INIT_VALUE) {
+    if (successor(stack, level, size)) {
+      if (valid(stack, level)) {
+        if (solution(level, size)) {
+          permutations++;
+          print_solution(stack, size);
+        } else {
+          level++;
+          stack[level] = STACK_INIT_VALUE;
+        }
+      } else {
+        ;
+      }
+    } else {
+      level--;
+    }
+  }
+
+  return permutations;
+}
+
+int main(void) {
+
+  int n = 0;
+
+  if (scanf("%d", &n) != 1)
+    return 1;
+
+  printf("%d", back(n));
+
+  return 0;
+}
